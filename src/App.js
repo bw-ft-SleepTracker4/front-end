9161fdae70a6a9bf1f344d39ff552dd2c6196263
Erick Canales <e94canales@gmail.com>
2020-04-27 11:21:15 -0700

    tweak

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
 import Navigation from '../src/components/Nav'
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
-      <Navigation />
+      <Navigation /> {/* GLOBAL NAVIGATION */}
 
       <Switch>
         <Route path='/login'>
