5d8f3737167d03c9826b503d9358a65f487a65bd
Erick Canales <e94canales@gmail.com>
2020-04-27 11:16:44 -0700

    added proper route paths to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import './App.css'
 import Navigation from '../src/components/Nav'
+import { Route, Switch, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <Navigation />
+    <div className='App'>
+      <Navigation />
+
+      <Switch>
+        <Route path='/login'>
+          <div>
+            SIGNIN
+          </div>
+        </Route>
+
+        <Route path='/signup'>
+          <div>
+            SIGNUP
+          </div>
+        </Route>
+
+        <Route path='/'>
+          <div>HOME</div>
+        </Route>
+          
+      </Switch>
+    </div>
+
+    
   );
+
 }
 
 export default App
